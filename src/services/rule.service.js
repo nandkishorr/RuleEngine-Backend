b80dffda87c84fdf90eaf6918ee27cd562663a8f
nandkishorr <rachulr7@gmail.com>
2024-10-27 03:08:50 +0530

    edit api created

--- src/services/rule.service.js
+++ src/services/rule.service.js
@@ -83,9 +83,32 @@ const create_rule = async (req,res) => {
           });
     }
  }
+ const update_rule = async (req, res) => {
+    try {
+        const ruleId = req.id;
+        const ruleData = req.rule;
+        const rule = await RuleModel.findById(ruleId);
+        if (!rule) {
+            return res.status(404).json({ message: 'Rule not found.' });
+        }
+        const ast = await buildAST(ruleData);
+        if (!ast) {
+            return res.status(400).json({ message: 'Invalid Rule' });
+        }
+        rule.rule = ruleData;
+        rule.ast = ast;
+        await rule.save();
+        return res.status(200).json({ message: 'Rule updated successfully.', rule });
+    } catch (error) {
+        console.error('Error occurred in update_rule:', error);
+        return res.status(500).json({ message: 'Failed to update rule', error: error.message });
+    }
+};
+
 module.exports = {
     create_rule,
     combine_rule,
     evaluate_rule,
-    get_all_rules
+    get_all_rules,
+    update_rule
 }
